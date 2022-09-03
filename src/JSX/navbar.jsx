c8a03c3912b621a22c2e60b246e565f1121b9bee
Alper <as.alper1@hotmail.com>
2022-09-03 10:45:06 +0300

    Navbar JSX v2

--- src/JSX/navbar.jsx
+++ src/JSX/navbar.jsx
@@ -1,11 +1,9 @@
 class Navbar extends React.Component {
 
     render() {
-      return <div className="w-full h-[6.5rem] bg-purple-600 px-4 flex items-center relative">
+      return <div className="w-full h-[5.8rem] bg-purple-600 px-4 flex items-center relative">
         <p className="text-3xl text-white font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">SQL Table Generator</p>
       </div>
     }
   }
   
-  const root = ReactDOM.createRoot(document.getElementById('navbar'));
-  root.render(<Navbar/>);
